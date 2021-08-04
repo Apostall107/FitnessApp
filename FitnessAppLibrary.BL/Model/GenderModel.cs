04aae9a38b0e80431cd5be13f3fe11d6fd79e82b
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-08-04 10:28:12 +0300

    GenderModel created

---
+++ FitnessAppLibrary.BL/Model/GenderModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessAppLibrary.BL.Model
+{
+
+    public class GenderModel
+    {
+
+        public string Name { get; set; }
+
+        public GenderModel(string name)
+        {
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+
+                throw new ArgumentNullException("Field cant be blank or null", nameof(name));
+
+            }
+
+            Name = name;
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+    }
+
+
+}
+
