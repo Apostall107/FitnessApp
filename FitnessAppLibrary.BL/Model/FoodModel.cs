a97b9807cd75bad432ac85cebe8ff0300ec8d12f
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-08-05 10:52:38 +0300

    FoodModel added.

---
+++ FitnessAppLibrary.BL/Model/FoodModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessAppLibrary.BL.Model
+{
+    public class FoodModel
+    {
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Proteins/100g of product.
+        /// </summary>
+        public double Protein { get; set; }
+
+        /// <summary>
+        /// Fats/100g of product.
+        /// </summary>
+        public double Fat { get; set; }
+
+        /// <summary>
+        /// Carbohydrates/100g of product.
+        /// </summary>
+        public double Carbohydrate { get; set; }
+
+        /// <summary>
+        /// Calories/100g of product.
+        /// </summary>
+        public double Calories { get; set; }
+
+        /// <summary>
+        ///  Energy in kilojoules/100g of product.
+        /// </summary>
+        public double Energy 
+        {
+            get
+            {
+
+                if (Calories > 0)
+                {
+                    return  Calories * 4.1868;
+                }
+                else
+                {
+
+                    return  0;
+
+                }
+
+            }
+        
+        }
+
+
+
+
+        public FoodModel() { }
+
+        public FoodModel(string name) : this(name, 0, 0, 0, 0) { }
+
+        public FoodModel(string name, double callories, double protein, double fat, double carbohydate)
+        {
+            // TODO: checks
+
+
+            Name = name;
+            Calories = callories / 100.0;
+            Protein = protein / 100.0;
+            Fat = fat / 100.0;
+            Carbohydrate = carbohydate / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+
+
+    }
+}
