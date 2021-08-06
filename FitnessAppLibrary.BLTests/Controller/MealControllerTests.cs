45ff639f1c810a1598ef81ccac322a1270090dd0
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-08-06 12:55:39 +0300

    MealController UnitTest added.

---
+++ FitnessAppLibrary.BLTests/Controller/MealControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FitnessAppLibrary.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FitnessAppLibrary.BL.Model;
+using System.Linq;
+
+namespace FitnessAppLibrary.BL.Controller.Tests
+{
+    [TestClass()]
+    public class MealControllerTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            //Arrange
+            string userName = Guid.NewGuid().ToString();
+            string foodName = Guid.NewGuid().ToString();
+            Random random = new Random();
+
+            UserController userController = new UserController(userName);
+            MealController mealController = new MealController(userController.CurrentUser);
+            FoodModel food = new FoodModel(foodName, random.Next(5, 500), random.Next(5, 500), random.Next(5, 500), random.Next(5, 500));
+
+            //Act
+            mealController.Add(food, 100);
+
+            //Assert
+            Assert.AreEqual(food.Name, mealController.Meal.FoodList.Last().Key.Name);
+
+        }
+    }
+}
