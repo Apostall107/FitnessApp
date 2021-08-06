2619c2f704b5273b0a5c8073e72b25a9a1199fa9
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-08-06 12:53:05 +0300

    BasicController abstract class created to simplify and optimize save/load code. Serialization algorithm extracted into separate class  SerrializeSaver.

---
+++ FitnessAppLibrary.BL/Controller/BasicController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace FitnessAppLibrary.BL.Controller
+{
+    public  abstract class BasicController
+    {
+        private readonly IDataSaver manager = new SerializeSaver();
+
+        protected void Save<T>(List<T> item) where T : class
+        {
+            manager.Save(item);
+        }
+
+        protected List<T> Load<T>() where T : class
+        {
+            return manager.Load<T>();
+        }
+
+
+
+    }
+}
---
+++ FitnessAppLibrary.BL/Controller/SerrializeSaver.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FitnessAppLibrary.BL.Controller
+{
+    public class SerializeSaver : IDataSaver
+    {
+        public List<T> Load<T>() where T : class
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string fileName = typeof(T).Name;
+
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                if (fs.Length > 0  && formatter.Deserialize(fs) is List<T> items)
+                {
+                    return items;
+                }
+                else
+                {
+                    return new List<T>();
+                }
+            }
+        }
+
+        public void Save<T>(List<T> item) where T : class
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string fileName = typeof(T).Name;
+
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, item);
+            }
+        }
+    }
+}
