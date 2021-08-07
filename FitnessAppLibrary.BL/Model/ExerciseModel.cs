992d63f2437bf4963c073abcfbb919cde0c26729
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-08-07 03:07:19 +0300

    Starting to realize physical activity logic. ExerciseModel and PrysicalActivityModels added.

---
+++ FitnessAppLibrary.BL/Model/ExerciseModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessAppLibrary.BL.Model
+{
+    public class ExerciseModel
+    {
+        // TODO: Look if needed to remake logic. Exercise model may contain calories per minute instead of PhysicalActivityModel and PhysActModel should be replaced with TrainigModel similar to FoodModel - MealModel.
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime FinishTime { get; set; }
+
+        public PhysicalActivityModel PhysicalActivity { get; set; }
+
+        public UserModel User { get; set; }
+
+        public ExerciseModel(DateTime startTime, DateTime finishTime, PhysicalActivityModel physicalActivity, UserModel user)
+        {
+            //TODO: checks.
+
+            StartTime = startTime;
+            FinishTime = finishTime;
+            PhysicalActivity = physicalActivity;
+            User = user;
+        }
+    }
+}
---
+++ FitnessAppLibrary.BL/Model/PhysicalActivityModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessAppLibrary.BL.Model
+{
+    public class PhysicalActivityModel
+    {
+
+        public string Name { get; set; }
+        /// <summary>
+        ///  Calories consumed per 1 minute of activity.
+        /// </summary>
+        public double CaloriesPerMinute { get; set; }
+
+        public PhysicalActivityModel(string name, double caloriesPerMinute)
+        {
+            // TODO: checks.
+
+            Name = name;
+            CaloriesPerMinute = caloriesPerMinute;
+        }
+
+
+        public override string ToString()
+        {
+            return "" ;
+        }
+
+
+    }
+}
