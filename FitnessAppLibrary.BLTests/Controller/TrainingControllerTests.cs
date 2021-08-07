853429cfe7cea8f53d9fdd2f953880595b03512a
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-08-07 04:08:56 +0300

    TrainingController module test added.

---
+++ FitnessAppLibrary.BLTests/Controller/TrainingControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FitnessAppLibrary.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FitnessAppLibrary.BL.Model;
+using System.Linq;
+
+namespace FitnessAppLibrary.BL.Controller.Tests
+{
+    [TestClass()]
+    public class TrainingControllerTests
+    {
+
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            //Arrange
+            string userName = Guid.NewGuid().ToString();
+            string exerciseName = Guid.NewGuid().ToString();
+            Random random = new Random();
+
+            UserController userController = new UserController(userName);
+            TrainingController trainingController = new TrainingController(userController.CurrentUser);
+            ExerciseModel exercise = new ExerciseModel(exerciseName, random.Next(5, 500));
+
+            //Act
+            trainingController.Add(exercise, DateTime.Now.AddHours(-2) , DateTime.Now.AddMinutes(-45) );
+
+            //Assert
+            Assert.AreEqual(exercise.Name, trainingController.exercisesList.Last().Name);
+            Assert.AreEqual(exercise.CaloriesPerMinute, trainingController.exercisesList.Last().CaloriesPerMinute);
+
+        }
+    }
+}
