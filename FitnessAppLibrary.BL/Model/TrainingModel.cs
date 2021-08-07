873ac106c2e010252b0d82b1406e07fb9f12a13e
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-08-07 04:06:30 +0300

    naming changes for more clear understanding: PhysicalActivityModel => TrainingModel.

---
+++ FitnessAppLibrary.BL/Model/TrainingModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessAppLibrary.BL.Model
+{
+    /// <summary>
+    /// Contains data of every training done.
+    /// </summary>
+    [Serializable]
+    public class TrainingModel
+    {
+        // TODO: Look if needed to remake logic. Exercise model may contain calories per minute instead of PhysicalActivityModel and PhysActModel should be replaced with TrainigModel similar to FoodModel - MealModel.
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime FinishTime { get; set; }
+
+        public ExerciseModel Exersice { get; set; }
+
+        public UserModel User { get; set; }
+
+        public TrainingModel(DateTime startTime, DateTime finishTime, ExerciseModel exercise, UserModel user)
+        {
+            //TODO: checks.
+
+            StartTime = startTime;
+            FinishTime = finishTime;
+            Exersice = exercise;
+            User = user;
+        }
+    }
+}
